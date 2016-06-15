c174e4ed237d4772514e82a9100dd23cf0422d19
Kyle Scales <kkscales0@gmail.com>
2016-06-15 15:55:51 -0400

    Write insertion-sort function.

--- main.cpp
+++ main.cpp
@@ -3,32 +3,60 @@
 
 using namespace std;
 
-void ALG1(vector<int> a); // INSERTION-SORT
-void ALG2(vector<int> a); // HEAPSORT
-void ALG3(vector<int> a); // QUICKSORT
+void PrintVector(vector<int> arr);
+
+void ALG1(vector<int> &arr); // INSERTION-SORT
+void ALG2(vector<int> arr); // HEAPSORT
+void ALG3(vector<int> arr); // QUICKSORT
+
+void InsertionSort(vector<int> &arr); // INSERTION-SORT
 
 int main() {
-    cout << "Hello, World!" << endl;
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8};
+    vector<int> arr = {19, 12, 1, 20, 100, 2, 3, 9};
+    //ALG2(arr);
+    PrintVector(arr);
     ALG1(arr);
+    PrintVector(arr);
+
     return 0;
 }
 
+void PrintVector(vector<int> arr) {
+    for(int & elements : arr) {
+        cout << elements << " ";
+    }
+    cout << endl;
+}
+
 // INSERTION-SORT
-void ALG1(vector<int> a) {
-    int n = a.size();
+void ALG1(vector<int> &arr) {
+    InsertionSort(arr);
+}
+
+// HEAPSORT
+void ALG2(vector<int> arr) {
+    int n = arr.size();
     cout << n << endl;
-    for(int & nums : a) {
+    for(int & nums : arr) {
         cout << nums << " ";
     }
 }
 
-// HEAPSORT
-void ALG2(vector<int> a) {
+// QUICKSORT
+void ALG3(vector<int> arr) {
 
 }
 
-// QUICKSORT
-void ALG3(vector<int> a) {
+void InsertionSort(vector<int> &arr) {
+    int key, i;
 
+    for(int j = 1; j <= arr.size() - 1; j++) {
+        key = arr[j];
+        i = j - 1;
+        while(i >= 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            i = i - 1;
+        }
+        arr[ i + 1] = key;
+    }
 }
