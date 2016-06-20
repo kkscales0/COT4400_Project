9631d9f6f4be34ee796cbc309a4fb2e6789f278a
Kyle Scales <kkscales0@gmail.com>
2016-06-19 20:52:43 -0400

    Implement and test Functions: ALG3, QuickSort, Partition, and SwapElementsInVector. Add timing to function calls.

--- main.cpp
+++ main.cpp
@@ -3,48 +3,54 @@
 
 using namespace std;
 
+vector<int> ALG1(vector<int> arr);    // INSERTION-SORT
+vector<int> ALG2(vector<int> arr);     // HEAPSORT
+vector<int> ALG3(vector<int> arr);     // QUICKSORT
+
+void InsertionSort(vector<int> &arr);           // INSERTION-SORT
+void QuickSort(vector<int> &arr, int beg, int end); // QUICK-SORT
+
+int Partition(vector<int> &arr, int beg, int end); // QUICK-SORT
+
 void PrintVector(vector<int> arr);
-
-void ALG1(vector<int> &arr); // INSERTION-SORT
-void ALG2(vector<int> arr); // HEAPSORT
-void ALG3(vector<int> arr); // QUICKSORT
-
-void InsertionSort(vector<int> &arr); // INSERTION-SORT
+void SwapElementsInVector(vector<int> &arr, int index1, int index2);
 
 int main() {
     vector<int> arr = {19, 12, 1, 20, 100, 2, 3, 9};
-    //ALG2(arr);
+    vector<int> sorted1, sorted3;
+    int startTime1, startTime3, endTime1, endTime3;
     PrintVector(arr);
-    ALG1(arr);
-    PrintVector(arr);
+
+    cout << "Insertion Sort" << endl;
+    startTime1 = clock();
+    sorted1 = ALG1(arr);
+    endTime1 = clock();
+    PrintVector(sorted1);
+    cout << "Time: " + to_string(endTime1 - startTime1) << endl;
+
+    cout << "Quick Sort" << endl;
+    startTime3 = clock();
+    sorted3 = ALG3(arr);
+    endTime3 = clock();
+    PrintVector(sorted3);
+    cout << "Time: " + to_string(endTime3 - startTime3) << endl;
 
     return 0;
 }
 
-void PrintVector(vector<int> arr) {
-    for(int & elements : arr) {
-        cout << elements << " ";
-    }
-    cout << endl;
-}
-
 // INSERTION-SORT
-void ALG1(vector<int> &arr) {
+vector<int> ALG1(vector<int> arr) {
     InsertionSort(arr);
+    return arr;
 }
 
 // HEAPSORT
-void ALG2(vector<int> arr) {
-    int n = arr.size();
-    cout << n << endl;
-    for(int & nums : arr) {
-        cout << nums << " ";
-    }
-}
+vector<int> ALG2(vector<int> arr) { }
 
 // QUICKSORT
-void ALG3(vector<int> arr) {
-
+vector<int> ALG3(vector<int> arr) {
+    QuickSort(arr, 0, arr.size() - 1);
+    return arr;
 }
 
 void InsertionSort(vector<int> &arr) {
@@ -59,4 +65,38 @@ void InsertionSort(vector<int> &arr) {
         }
         arr[ i + 1] = key;
     }
+}
+
+void QuickSort(vector<int> &arr, int beg, int end) {
+    if(beg < end) {
+        int q = Partition(arr, beg, end);
+        QuickSort(arr, beg, q - 1);
+        QuickSort(arr, q + 1, end);
+    }
+}
+
+int Partition(vector<int> &arr, int beg, int end) {
+    int q = arr[end];
+    int i = beg - 1;
+    for(int j = beg; j < end; j++) {
+        if(arr[j] <= q) {
+            i = i + 1;
+            SwapElementsInVector(arr, i, j);
+        }
+    }
+    SwapElementsInVector(arr, i + 1, end);
+    return i + 1;
+}
+
+void PrintVector(vector<int> arr) {
+    for(int & elements : arr) {
+        cout << elements << " ";
+    }
+    cout << endl;
+}
+
+void SwapElementsInVector(vector<int> &arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
 }
